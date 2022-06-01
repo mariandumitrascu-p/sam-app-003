558c2814674c5e6c3c47d0062fbde86f8c71cdef
Marian Dumitrascu <md@dumitrascu.net>
2022-06-01 18:03:48 -0500

    ch

--- _history.sh
+++ _history.sh
@@ -26,21 +26,18 @@ AWS_SAM_STACK_NAME=<stack-name> python -m pytest tests/integration -v
 
 aws lambda invoke --function-name testFunction --cli-binary-format raw-in-base64-out --payload '{"name": "John Smith"}' response.json
 
-
 aws lambda invoke \
     --function-name arn:aws:lambda:us-east-1:327004596447:function:sam-app-HelloWorldFunction-W1OJIH1QSESm  \
     --cli-binary-format raw-in-base64-out \
     --payload file:///Users/marian.dumitrascu/Dropbox/Work/Current/lambda/SAM/sam-app-003/events/event02.json \
     response.json
 
-
-
 aws cloudformation delete-stack --stack-name sam-app
 
 mariandumitrascu-p/sam-app-003
 
 # https://aws.amazon.com/blogs/compute/introducing-aws-sam-pipelines-automatically-generate-deployment-pipelines-for-serverless-applications/
-# https://aws.amazon.com/blogs/compute/introducing-aws-sam-pipelines-automatically-generate-deployment-pipelines-for-serverless-applications/
+# https://haydnjmorris.medium.com/solving-permissions-error-with-aws-codepipeline-c93cfc000285
 
-sam deploy -t codepipeline.yaml --stack-name sam-app --capabilities=CAPABILITY_IAM --region us-east-1
+sam deploy -t codepipeline.yaml --stack-name sam-app-pipeline-001 --capabilities=CAPABILITY_IAM --region us-east-1
 
